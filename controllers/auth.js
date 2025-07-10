4d99eb8875408ca69373c962dab20217c29372b8
Mohankumar <1726k0146@gmail.com>
2025-07-10 14:49:01 +0530

    changed

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,8 +19,8 @@ exports.registerUser = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false, // set to true in production with HTTPS
-      sameSite: 'Lax',
+      secure: true, // set to true in production with HTTPS
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
