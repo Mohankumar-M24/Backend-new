14e106877609b805928ef4bbd20873de78985e06
Mohankumar <1726k0146@gmail.com>
2025-07-11 09:40:34 +0530

    changes done

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // ✅ CORS middleware
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://peaceful-cat-22ce03.netlify.app', // e.g., https://courageous-horse-18f999.netlify.app
+  'https://effervescent-muffin-f69cf4.netlify.app', // e.g., https://courageous-horse-18f999.netlify.app
 ];
 
 app.use(cors({
